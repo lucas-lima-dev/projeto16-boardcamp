038aa1c895a95aaafb1cfb47f4858b703e003e01
lucas-lima-dev <lucas.sdslima@gmail.com>
2023-02-08 22:28:56 -0300

    Requisito: CRUD de clientes Create Read and Update sem validacoes

--- src/controllers/clientes.controllers.js
+++ src/controllers/clientes.controllers.js
@@ -1,1 +1,61 @@
 import { db } from "../database/database.connection";
+
+export async function searchCustomers(req, res) {
+  try {
+    const customers = await db.query("SELECT * FROM customers");
+    res.send(customers);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function searchCustomersById(req, res) {
+  const { id } = req.params;
+
+  try {
+    const customers = await db.query(
+      `
+        SELECT * FROM customers
+        WHERE customers.id = $1`,
+      [id]
+    );
+    res.send(customers);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function addCustomers(req, res) {
+  const { name, phone, cpf, birthday } = req.body;
+
+  try {
+    await db.query(
+      `
+        INSERT INTO customers (name, phone,cpf,birthday) 
+        VALUES ($1,$2,$3,$4)`,
+      [name, phone, cpf, birthday]
+    );
+
+    res.status(201);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function updateCustomers(req, res) {
+  const { name, phone, cpf, birthday, id } = req.body;
+
+  try {
+    await db.query(
+      `
+          UPDATE customers 
+          SET name=$1, phone=$2,cpf=$3,birthday=$4 
+          WHERE id=$5`,
+      [name, phone, cpf, birthday, id]
+    );
+
+    res.status(200);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
--- src/controllers/jogos.controllers.js
+++ src/controllers/jogos.controllers.js
@@ -1,26 +1,27 @@
 import { db } from "../database/database.connection";
 
-export async function searchGames(req,res) {
-    try {
-        const games = await db.query("SELECT * FROM games")
-        res.send(games)
-    } catch (error) {
-        res.status(500).send(error.message)
-    }
+export async function searchGames(req, res) {
+  try {
+    const games = await db.query("SELECT * FROM games");
+    res.send(games);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 }
 
-export async function addGames(req,res) {
+export async function addGames(req, res) {
+  const { name, image, stockTotal, pricePerDay } = req.body;
 
-    const {name,image,stockTotal,pricePerDay} = req.body
-
-    try {
-        await db.query(`
+  try {
+    await db.query(
+      `
         INSERT INTO games (name, image,stockTotal,pricePerDay) 
         VALUES ($1,$2,$3,$4)`,
-        [name,image,stockTotal,pricePerDay])
+      [name, image, stockTotal, pricePerDay]
+    );
 
-        res.status(201)
-    } catch (error) {
-        res.status(500).send(error.message)
-    }
+    res.status(201);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 }
