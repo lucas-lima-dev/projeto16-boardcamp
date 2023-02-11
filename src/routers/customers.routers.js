3d6eb2ee4a01a0796c21dc0ab67792afbeb7f023
lucas-lima-dev <lucas.sdslima@gmail.com>
2023-02-11 15:50:52 -0300

    Fix: Requisito CRUD Clientes: corrigindo validacao no schema de criar um novo cliente com numero de cpf maior que 11 numeros

--- src/routers/customers.routers.js
+++ src/routers/customers.routers.js
@@ -2,13 +2,15 @@ import { Router } from "express";
 import { 
     searchCustomers,
     addCustomers,
-    updateCustomers 
+    updateCustomers, 
+    searchCustomersById
 } from "../controllers/customers.controllers.js";
 import { addCustomersValidation, updateCustomersValidation } from "../middlewares/customers.middleware.js";
 
 const customersRouter = Router();
 
 customersRouter.get("/customers",searchCustomers)
+customersRouter.get("/customers/:id",searchCustomersById)
 customersRouter.post("/customers",addCustomersValidation,addCustomers)
 customersRouter.put("/customers/:id",updateCustomersValidation,updateCustomers)
 
